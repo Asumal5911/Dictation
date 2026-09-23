8a16c58e0ec7c0408324e14cc64e6d4c7b87bd1c
Angad Sumal <angadsumal@gmail.com>
2026-09-23 13:53:38 -0500

    Clean up repository: Keep only final integration source code, ignore compiled binaries

--- Integration/test_speaker.py
+++
@@ -1,7 +0,0 @@
-import torch
-import torchaudio
-from speechbrain.inference.speaker import EncoderClassifier
-
-print("Loading model...")
-classifier = EncoderClassifier.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb", savedir="tmpdir")
-print("Model loaded.")
--- Phase2_Proof/Shared.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-@objc protocol RecorderXPCProtocol {
-    func startRecording(reply: @escaping (Bool, String?) -> Void)
-    func stopRecording(reply: @escaping (Bool) -> Void)
-    func getStatus(reply: @escaping (String) -> Void)
-}
